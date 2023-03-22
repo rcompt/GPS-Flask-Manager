f059f956c8de4be70565332c3ecc1d88f7323b60
Ryan Compton <ryancompton.james@gmail.com>
2023-03-21 23:26:37 -0700

    Added GET function for GPS boards

--- gps_server.py
+++ gps_server.py
@@ -151,6 +151,27 @@ def locationaddbulk():
     return jsonify(
         {"RESPONSE" : f"Successfully added: {locations_count} locations"}
     )
+    
+@app.route("/retrieve", methods=["GET"])
+def get():
+    row_id = request.args.get('row_id')
+    return jsonify(
+        {
+            "RESPONSE" : f"{row_id}, here is your data",
+            "DATA" : [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ]
+        }
+    )
 
 @app.route("/update", methods=["POST"])
 def update():
