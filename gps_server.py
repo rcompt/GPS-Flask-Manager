ba9d78a3dd990e3c595c222b338eead62cb96807
Ryan Compton <ryancompton.james@gmail.com>
2023-03-27 21:15:37 -0700

    Updated retreive method to send row id

--- gps_server.py
+++ gps_server.py
@@ -159,16 +159,7 @@ def get():
         {
             "RESPONSE" : f"{row_id}, here is your data",
             "DATA" : [
-                0,
-                1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                7,
-                8,
-                9
+                int(row_id)
             ]
         }
     )
